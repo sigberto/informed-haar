353ff57855a99e170476b9bb5d13b249c9f51f39
bgarcia7 <bgarcia7@stanford.edu>
2016-06-05 00:21:43 -0700

    added updated script

--- opt_informed-haar.py
+++ opt_informed-haar.py
@@ -1,9 +1,9 @@
 from opt_pipeline import Pipeline
 
-data_file = 'top_100_ft_vectors.p'
-model_file = 'top_1000_ft_classifier.p'
+# data_file = 'top_100_ft_vectors.p'
+model_file = 'top_ft_classifier_1000'
 
-pipe = Pipeline()
-X, Y = pipe.extract_features(file_name=data_file)
+# pipe = Pipeline()
+# X, Y = pipe.extract_features(file_name=data_file)
 pipe.train(X, Y, num_estimators=2000, max_depth=2, model_name=model_file)
 pipe.detect(clf=model_file)
